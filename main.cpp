3477eea027a88a70e231eb1813504a0631241797
Rex Oliver <rexroao@g.clemson.edu>
2021-03-29 18:23:31 -0400

    started main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Main file for 
+
+int main(){
+    
+    
+    getUsers();
+    cout << "Select User: " << endl;
+    
+
+    cout << "Enter email of the administrator we want to send our email to: ";
+     << ""
+
+
+}
+
+vector<> getUsers(){
+
+}
+
